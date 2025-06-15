d2dda02602550cf27a579e0b2d7ceb864b5d913a
pulitano <andersonpulitano@gmail.com>
2025-06-15 12:10:12 -0300

    complemento da logica

--- desafio_sistema_bancario_1.py
+++ desafio_sistema_bancario_1.py
@@ -7,8 +7,8 @@ menu = """
 
 """
 
-saldo = 0
-limite = 500
+saldo = 0.0
+limite = 500.0
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
@@ -17,13 +17,45 @@ while True:
     opcao = input(menu)
 
     if opcao == "1":
-        print("Deposito")
+
+        valor = float(input("VALOR PARA DEPOSITO: "))
+        saldo += valor
+        extrato += f"DEPOSITO: R$ {valor:.2f}\n"
+        print(f"\nSALDO ATUAL: R$ {saldo:.2f}")
+
     elif opcao == "2":
-        print("Saque")
+
+        if numero_saques < LIMITE_SAQUES:
+
+            valor = float(input("VALOR DO SAQUE:"))
+
+            if valor <= saldo and valor <= limite:
+
+                saldo -= valor
+                numero_saques += 1
+                extrato += f"SAQUE: R$ {valor:.2f}\n"
+                print(f"\nSALDO ATUAL: R$ {saldo:.2f}")
+
+            elif valor > saldo:
+                print(f"\nERRO -> Sem saldo suficiente.\nSALDO ATUAL: R$ {saldo:.2f}")
+            else:
+                print(f"\nERRO -> Valor maior que o limite autorizado.\nLIMITE: R$ {limite:.2f}")
+            
+        else:
+            print("\nERRO -> Limite de saques atingido.")
+
     elif opcao == "3":
-        print("Extrato")
+        
+        if not extrato:
+            print("EXTRATO VAZIO -> SEM OPERAÇÕES")
+        else:
+            print("--- EXTRATO DAS OPERAÇÕES ---")
+            extrato += "-----------------------------\n"
+            extrato += f"SALDO ATUAL: R$ {saldo:.2f}"
+            print(extrato)
+
     elif opcao == "0":
         break
     else:
-        print("X Operação invalida X - Selecione novamente a opção desejada.")
+        print("Operação invalida - Selecione novamente a opção desejada.")
     
